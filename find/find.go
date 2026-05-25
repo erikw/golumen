cc0296ec4ff4fe59fc5c8ca7041dc4652e0c9c6a
Erik Westrup <erik.westrup@icloud.com>
2026-05-25 15:51:10 +0200

    Add TODO

--- find/find.go
+++ find/find.go
@@ -56,6 +56,7 @@ func (f *Finder) Find(path string, pattern string) (matches []string, err error)
 	return fc.matches, nil
 }
 
+// TODO paralellize with a set of worker go routines?
 func (fc *findCollector) walk(path string, isRoot bool) error {
 	info, err := os.Lstat(path)
 	if err != nil {
